f5c9bc44674b05fcdb74f80f337bfebcae83a599
lnimmagadda <lnimmagadda@newrelic.com>
2026-02-26 19:06:13 +0530

    Final

--- plugins/fix-remark-path-prefix-plugin/index.js
+++ plugins/fix-remark-path-prefix-plugin/index.js
@@ -4,7 +4,7 @@ module.exports = ({ markdownAST, pathPrefix }, { basePath = ''}) => {
   // https://github.com/gatsbyjs/gatsby/issues/38362
 
   if (pathPrefix?.match('^(https?:\\/\\/)?')) {
-    const url = pathPrefix.replace(/(http(s)*:)\/\//, `$1/`);
+    const url = pathPrefix.replace(/(http(s)*:)\/\//, `$1//`);
     visit(markdownAST, 'link', node => {
       if (node.url.includes(url)) {
         const newUrl = node.url.replace(url, basePath) || '/';
